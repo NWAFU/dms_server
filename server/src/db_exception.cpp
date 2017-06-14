c54340642058912bd5e482c0f54a98aaa149c0af
Ma Jun <974830081@qq.com>
2017-06-14 09:16:28 +0800

    帯注释服务器异常类

--- server/src/db_exception.cpp
+++ server/src/db_exception.cpp
@@ -1,12 +1,26 @@
 #include"header/db_exception.h"
 
-
-DBException::DBException()
+/**************************************************
+*作者：马骏
+*函数名：DBException()
+*功能：数据库异常类默认构造函数
+*输入参数：none
+*输出参数：none
+*返回值：none
+**************************************************/
+DBException::DBException()  :ServerException("数据库异常")
 {
-    ServerException("数据库异常");
-}
 
-DBException::DBException(const std::string& msg) : ServerException(msg)
+}
+/**************************************************
+*作者：马骏
+*函数名：DBException(const string& msg)
+*功能：数据库异常类构造函数
+*输入参数：const string& msg
+*输出参数：none
+*返回值：none
+**************************************************/
+DBException::DBException(const string& msg) : ServerException(msg)
 {
 
 }
--- server/src/socket_exception.cpp
+++ server/src/socket_exception.cpp
@@ -1,11 +1,25 @@
 #include"header/socket_exception.h"
-using std::string;
+/**************************************************
+*作者：马骏
+*函数名：SocketException()
+*功能：套接字异常类默认构造函数
+*输入参数：none
+*输出参数：none
+*返回值：none
+**************************************************/
 
-SocketException::SocketException()
+SocketException::SocketException() :ServerException("套接字异常")
 {
-    ServerException("套接字异常");
-}
 
+}
+/**************************************************
+*作者：马骏
+*函数名：SocketException(const string& msg)
+*功能：套接字异常类帯参数构造函数
+*输入参数：const string& msg
+*输出参数：none
+*返回值：none
+**************************************************/
 SocketException::SocketException(const string& msg) : ServerException(msg)
 {
 
