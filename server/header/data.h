788f8b7c1a8d2376af8786a6557f82c18552d6f8
Chaoyang Liu <chaoyanglius@gmail.com>
2017-06-15 15:44:47 +0800

    correct data.h

--- server/header/data.h
+++ server/header/data.h
@@ -3,15 +3,17 @@
 
 #include <unistd.h>//pid_t
 #include <fstream>//ofstream
+#include <iostream>//ostream
 
 using std::ifstream;
 using std::ofstream;
+using std::ostream;
 
 struct LogRec
 {
     char log_name[32];
     pid_t pid;
-    long log_time;
+    int log_time;
     char log_ip[32];
     short log_type;
 };
@@ -20,12 +22,13 @@ struct MatchedLogRec
 {
     char log_name[32];
     pid_t pid;
-    long login_time;
-    long logout_time;
-    long duration;
+    int login_time;
+    int logout_time;
+    int duration;
     char log_ip[32];
     friend ifstream & operator>>(ifstream & fin,MatchedLogRec & matched_log);
     friend ofstream & operator<<(ofstream & fout,MatchedLogRec const & matched_log);
+    friend ostream & operator<<(ostream & out,MatchedLogRec const & matched_log);
 };
 
 #endif // DATA_H
